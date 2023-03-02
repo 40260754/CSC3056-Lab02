fcbe5839112f60cb9f31ed1728edb280312e8dc5
40260754 <40260754@ecs2-4001.eeecs.qub.ac.uk>
2023-03-02 11:24:06 +0000

    made class path relative

--- src/org/jfree/data/test/RangeTest.java
+++ src/org/jfree/data/test/RangeTest.java
@@ -36,26 +36,51 @@ public class RangeTest extends TestCase {
 	}
 
 	public void testGetLengthBothPositiveAndEqual() {
-	    Range r1 = new Range(2, 2);
-	    assertEquals("getLength: Did not return the expected output", 0.0, r1.getLength());
+		Range r1 = new Range(2, 2);
+		assertEquals("getLength: Did not return the expected output", 0.0, r1.getLength());
 	}
+
 	public void testGetLengthBothPositiveAndNotEqual() {
-	    Range r2 = new Range(4, 9);
-	    assertEquals("getLength: Did not return the expected output", 5.0, r2.getLength());
+		Range r2 = new Range(4, 9);
+		assertEquals("getLength: Did not return the expected output", 5.0, r2.getLength());
 	}
+
 	public void testGetLengthBothNegativeAndEqual() {
-	    Range r3 = new Range(-99, -99);
-	    assertEquals("getLength: Did not return the expected output", 0.0, r3.getLength());
+		Range r3 = new Range(-99, -99);
+		assertEquals("getLength: Did not return the expected output", 0.0, r3.getLength());
 	}
+
 	public void testGetLengthBothNegativeAndNotEqual() {
-	    Range r4 = new Range(-11, -4);
-	    assertEquals("getLength: Did not return the expected output", 7.0, r4.getLength());
+		Range r4 = new Range(-11, -4);
+		assertEquals("getLength: Did not return the expected output", 7.0, r4.getLength());
 	}
+
 	public void testGetLengthOnePositiveOneNegative() {
-	    Range r5 = new Range(-5, 3);
-	    assertEquals("getLength: Did not return the expected output", 8.0, r5.getLength());
-	    
-	    //TODO Pick 5 other methods in Range and have design tests for them
+		Range r5 = new Range(-5, 3);
+		assertEquals("getLength: Did not return the expected output", 8.0, r5.getLength());
+
+		// TODO Pick 5 other methods in Range and have design tests for them
 	}
+
+	public void testContainsFirstValue() {
+		Range r6 = new Range(-5, 3);
+		assertEquals("Contains: Did not return the expected output", true, r6.contains(-5));
+	}
+
+	public void testContainsLastValue() {
+		Range r7 = new Range(-5, 3);
+		assertEquals("Contains: Did not return the expected output", true, r7.contains(3));
+	}
+
+	public void testContainsDoesNotContain() {
+		Range r8 = new Range(-5, 3);
+		assertEquals("Contains: Did not return the expected output", false, r8.contains(8));
+	}
+/*
+	public void testContainsEmptyRange() {
+		Range r9 = new Range(-5, -5);
+		assertEquals("Contains: Did not return the expected output", false, r9.contains(-5));
+	}
+	*/
 
 }
